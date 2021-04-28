f3f2568da7d04f07f4573ed61a9ec3bc1e53a845
nelsonsouzajr <nelson.souzajunior@gmail.com>
2021-04-28 16:15:13 -0300

    first example of a notification

--- app/src/main/java/com/dostoi/aplicationnotification/MainActivity.kt
+++ app/src/main/java/com/dostoi/aplicationnotification/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.dostoi.aplicationnotification
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnSend: Button
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnSend = findViewById(R.id.button_send_notification)
         btnSend.setOnClickListener {
-
+            this.showNotification("1234", "bootcamp Android", "Kotlin Android curso")
         }
     }
 }
--- app/src/main/java/com/dostoi/aplicationnotification/NotificationUtils.kt
+++ app/src/main/java/com/dostoi/aplicationnotification/NotificationUtils.kt
@@ -5,6 +5,11 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 
 lateinit var notificationChannel: NotificationChannel
@@ -12,8 +17,28 @@ lateinit var notificationManager: NotificationManager
 lateinit var builder: NotificationCompat.Builder
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 fun Context.showNotification(channelId: String, title: String, body: String) {
     notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     val intent = Intent(this, MainActivity::class.java)
     val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-}
+    //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.0) {
+        notificationChannel = NotificationChannel(channelId, body, NotificationManager.IMPORTANCE_HIGH).apply {
+            lightColor = Color.BLUE
+            enableVibration(true)
+       }
+
+
+        notificationManager.createNotificationChannel(notificationChannel)
+            builder = NotificationCompat.Builder(this, channelId).apply {
+                setSmallIcon(R.drawable.ic_refresh)
+                setContentTitle(title)
+                setContentText(body)
+                setAutoCancel(true)
+                setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE))
+                setContentIntent(pendingIntent)
+            }
+    notificationManager.notify(channelId.toInt(), builder.build())
+    }
+
+
